9c6a7c2c05223daf621386167de33373dc94d626
bekhruz_web <bexruzizzatullayev@gmail.com>
2025-09-09 14:33:10 +0500

    some changes

--- src/main/java/com/example/springcore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/springcore/exception/GlobalExceptionHandler.java
@@ -2,11 +2,19 @@ package com.example.springcore.exception;
 
 import com.example.springcore.dto.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j    // -->> bu logging uchun annotation
@@ -32,4 +40,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 : throwable.getLocalizedMessage()) : getLastCause(throwable.getCause());
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
